fe617c2b05a08fbd006812cfeab0ff1c75895201
kushniryuk <kusniryukigor@gmail.com>
2023-10-04 14:40:49 +0300

    Add files via upload

---
+++ RestartWinmgmtHighLoadCpu.ps1
@@ -0,0 +1,32 @@
+﻿$lastRestartTime = $null
+
+while ($true) {
+    $cpuLoad = Get-WmiObject -Query "SELECT LoadPercentage FROM Win32_Processor" | Select-Object -ExpandProperty LoadPercentage
+
+    if ($cpuLoad -gt 90) {
+        if ($lastRestartTime -ne $null) {
+            $uptimeMinutes = [math]::Round(([datetime]::Now - $lastRestartTime).TotalMinutes)
+            Write-Host "Загрузка процессора превышает 90% ($cpuLoad%). Прошло $uptimeMinutes минут с последней перезагрузки."
+        }
+        else {
+            Write-Host "Загрузка процессора превышает 90% ($cpuLoad%). Первая проверка."
+        }
+
+        Write-Host "Выполняю перезапуск службы winmgmt..."
+        Restart-Service -Name winmgmt -Force
+        $lastRestartTime = Get-Date
+    }
+    else {
+        $currentTime = Get-Date
+        if ($lastRestartTime -ne $null) {
+            $uptimeMinutes = [math]::Round(([datetime]::Now - $lastRestartTime).TotalMinutes)
+            Write-Host "Всё ОК. Загрузка процессора: $cpuLoad%. Прошло $uptimeMinutes минут с последней перезагрузки."
+        }
+        else {
+            Write-Host "Всё ОК. Загрузка процессора: $cpuLoad%. Первая проверка."
+        }
+        
+        Write-Host "Ждём 5 минут..."
+        Start-Sleep -Seconds 300
+    }
+}
---
+++ shadowRDP.ps1
@@ -0,0 +1,20 @@
+﻿while ($true) {
+    Clear-Host
+    $quserInfo = quser
+    Write-Host "Список текущих сеансов пользователей:"
+    quser
+
+    # Запрос пользователя на выбор ID сеанса для подключения
+    $selectedId = Read-Host "Введите ID пользователя для подключения"
+
+    # Проверка, что введенный ID не пуст и существует в списке сеансов
+    if (-not [string]::IsNullOrWhiteSpace($selectedId) -and $quserInfo -match "\b$selectedId\b") {
+        # Подключение к выбранному сеансу с использованием mstsc
+        $mstscCommand = "Mstsc /shadow:$selectedId /control /noConsentPrompt"
+        Invoke-Expression $mstscCommand
+    } elseif ([string]::IsNullOrWhiteSpace($selectedId)) {
+        Write-Host "В начало скрипта"
+    } else {
+        Write-Host "В начало скрипта"
+    } 
+}
